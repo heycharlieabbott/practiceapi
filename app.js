a59d4c59247da810d985988863e3e321dc2dfd16
Charlie Abbott <chzabbott@gmail.com>
2023-08-22 00:40:34 +0900

    add mongoose and begin route updating

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
+const connectDB = require('./connect')
+require('dotenv').config()
+
 const express = require('express');
 const app = express();
 let {people} = require('./data');
+
+const {createTask, getAllTasks, getTask, updateTask, deleteTask} = require('./controllers/tasks')
 
 app.get('/api/people',(req,res)=>{
     res.status(200).json({success: true, data: people})
 })
 
-app.listen(5001, () =>{
-    console.log('app is listening on 5001')
-})
 
+
+
+const start = async () =>{
+try {
+    await connectDB(process.env.MONGO_URI)
+    app.listen(5001, () =>{
+        console.log('app is listening on 5001')
+    })
+} catch (error) {
+    console.log(error)    
+}
+}
+
+start()
+app.get('/api',getTask)
